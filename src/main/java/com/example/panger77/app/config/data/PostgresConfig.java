9503647ca7302044aa448deb5db7291c1a2efe74
Dany <peiler.dany@gmail.com>
2022-06-15 05:19:27 +0300

    back step

--- src/main/java/com/example/panger77/app/config/data/PostgresConfig.java
+++ src/main/java/com/example/panger77/app/config/data/PostgresConfig.java
@@ -9,23 +9,13 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PostgresConfig {
-//    @Bean
-//    public DataSource getDataSource() throws BeanCreationException {
-//        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-//        dataSourceBuilder.driverClassName("org.postgresql.Driver");
-//        dataSourceBuilder.url("jdbc:postgresql://localhost:5432/postgres");
-//        dataSourceBuilder.username("postgres");
-//        dataSourceBuilder.password("postgres");
-//        return dataSourceBuilder.build();
-//    }
     @Bean
     public DataSource getDataSource() throws BeanCreationException {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("org.postgresql.Driver");
-        dataSourceBuilder.url("jdbc:postgresql://ec2-52-30-67-143.eu-west-1.compute.amazonaws.com/dmt5imce6dlrn");
-        dataSourceBuilder.username("zlhkerasqtnjlo");
-        dataSourceBuilder.password("d4dc87edaf1ac8d01e362614bfc762036c833971a34284b536dd5410da9f7fc5");
+        dataSourceBuilder.url("jdbc:postgresql://localhost:5432/postgres");
+        dataSourceBuilder.username("postgres");
+        dataSourceBuilder.password("postgres");
         return dataSourceBuilder.build();
     }
-
 }
